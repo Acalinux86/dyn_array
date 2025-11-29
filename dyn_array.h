c7e25ef2cb68e90ee2d1a0094e9163dbaaa3e249
Ahmed Ali <ahmedcali1984@gmail.com>
2025-11-29 19:42:32 +0300

    [dyn_array] Add Dynamic Array Fill and Resize Methods

--- dyn_array.h
+++ dyn_array.h
@@ -3,6 +3,7 @@
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #define DYN_ARRAY_INITIAL_CAPACITY 256
 
@@ -41,9 +42,31 @@
         if ((dyn_array)->count == 0) {                                  \
             fprintf(stderr,                                             \
                     "Warning: Attempting to Pop From An empty Dynamic Array.\n"); \
-            break;                                                      \
+            abort();                                                    \
         } else {                                                        \
             dyn_array_delete_content((dyn_array), (dyn_array)->count - 1); \
+        }                                                               \
+    } while (0)
+
+// NOTE: Resizes the Dynamic Array, making the new capacity
+// double of the old one
+#define dyn_array_resize(dyn_array)                                     \
+    do {                                                                \
+        if ((dyn_array)->count >= (dyn_array)->capacity) {              \
+            (dyn_array)->capacity =                                     \
+                (dyn_array)->capacity == 0 ?                            \
+                DYN_ARRAY_INITIAL_CAPACITY :                            \
+                ((dyn_array)->capacity * 2 + 1);                        \
+            /* Reallocate the array contents to the new capacity*/      \
+            (dyn_array)->contents =                                     \
+                realloc(                                                \
+                    (dyn_array)->contents,                              \
+                    sizeof((dyn_array)->contents)*((dyn_array)->capacity)); \
+            /* Check if The Reallocation pointer is not NULL */         \
+            if ((dyn_array)->contents == NULL ) {                       \
+                fprintf(stderr, "ERROR: Memory Reallocation For Dynamic Array Failed."); \
+                abort(); /* abort*/                                     \
+            }                                                           \
         }                                                               \
     } while (0)
 
@@ -51,35 +74,22 @@
 #define dyn_array_push(dyn_array, content, index)                       \
     do {                                                                \
         /* Check if the array needs resizing*/                          \
-        if ((dyn_array)->count >= (dyn_array)->capacity) {              \
-            (dyn_array)->capacity =                                     \
-                (dyn_array)->capacity == 0 ?                            \
-                DYN_ARRAY_INITIAL_CAPACITY :                            \
-                (dyn_array)->capacity * 2 + 1;                          \
-            /* Reallocate the array contents to the new capacity*/      \
-            (dyn_array)->contents =                                     \
-                realloc(                                                \
-                    (dyn_array)->contents,                              \
-                    sizeof(*(dyn_array)->contents)*(dyn_array)->capacity \
-                    );                                                  \
-            /* Assert The Reallocation pointer is not NULL */           \
-            assert((dyn_array)->contents != NULL &&                     \
-                   "Memory Reallocation For Dynamic Array Failed.");    \
-        }                                                               \
+        dyn_array_resize(dyn_array);                                    \
         /* Shift the elements if insert is happening at a specific
-           index */                                                 \
+           index */                                                     \
         if (index != (dyn_array)->count) {                              \
             for (size_t i = (dyn_array)->count; i > index; --i) {       \
-                (dyn_array)->contents[i] = (dyn_array)->contents[i - 1];    \
-            }                                                       \
-        }                                                           \
-        /* Increase Dynamic Array count and append the content  */  \
+                (dyn_array)->contents[i] = (dyn_array)->contents[i - 1]; \
+            }                                                           \
+        }                                                               \
+        /* Increase Dynamic Array count and append the content  */      \
         (dyn_array)->contents[index] = content;                         \
         (dyn_array)->count++;                                           \
     } while (0)
 
 // NOTE: Append An Content in the end
-#define dyn_array_append(dyn_array, content) dyn_array_push((dyn_array), content, (dyn_array)->count)
+#define dyn_array_append(dyn_array, content) \
+    dyn_array_push((dyn_array), content, (dyn_array)->count)
 
 // NOTE: Destroy the Dynamic Array
 #define dyn_array_delete(dyn_array)             \
@@ -89,5 +99,13 @@
         (dyn_array)->capacity = 0;              \
         (dyn_array)->contents = NULL;           \
     } while(0)
+
+
+#define dyn_array_fill(dyn_array, count, value) \
+    do {                                        \
+        for (size_t i = 0; i < count; ++i) {    \
+            dyn_array_append(dyn_array, value); \
+        }                                       \
+    } while (0)
 
 #endif // DYN_ARRAY_H_
